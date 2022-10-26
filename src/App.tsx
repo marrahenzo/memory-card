4ed3c9b3b3a6d04dceba767f53a158da31d5d730
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-26 18:38:03 -0300

    Basic app layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Counter from './components/Counter';
+import './styles/App.css';
+import logo from './media/images/smitelogo.png';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div id='header'>
+        <img src={logo} alt='Smite logo' />
+      </div>
+      <div id='counter-container'>
+        <Counter text='Current score' value={0} />
+        {/* TODO: get value from localStorage */}
+        <Counter text='Best score' value={0} />
+      </div>
+      <div id='card-container'>example card</div>
+    </div>
+  );
 }
 
 export default App;
