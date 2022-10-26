77cab330478e18d2fb0fb247b3ab8630e588e5fc
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-26 19:18:02 -0300

    Add score and best score state

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import Counter from './components/Counter';
 import './styles/App.css';
 import logo from './media/images/smitelogo.png';
 import background from './media/images/background.jpg';
+import React, { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState(0);
+  /* TODO: get value from localStorage */
+  const [best, setBest] = useState(0);
+
   return (
     <div className='App'>
       <img src={background} alt='Background' id='background-image' />
