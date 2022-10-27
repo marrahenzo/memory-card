8897fb3067a585bf59c4845731a3fc72a8cdf8bb
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-26 21:27:26 -0300

    Create character image import index

---
+++ src/gods.ts
@@ -0,0 +1,8 @@
+import Apollo from './media/images/apollo.jpg';
+import Mercury from './media/images/mercury.jpg';
+import Bellona from './media/images/bellona.jpg';
+import Hebo from './media/images/hebo.jpg';
+
+const gods = { Apollo, Mercury, Bellona, Hebo };
+
+export default gods;
