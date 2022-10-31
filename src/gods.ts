3ad889b36cf13cec287fe21eb53853ed51f28711
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-30 22:08:09 -0300

    Add class and redo array

--- src/gods.ts
+++ src/gods.ts
@@ -3,6 +3,21 @@ import Mercury from './media/images/mercury.jpg';
 import Bellona from './media/images/bellona.jpg';
 import Hebo from './media/images/hebo.jpg';
 
-const gods = { Apollo, Mercury, Bellona, Hebo };
+export class God {
+  name: string;
+  image: string;
+
+  constructor(name: string, image: string) {
+    this.name = name;
+    this.image = image;
+  }
+}
+
+export const gods = [
+  new God('Apollo', Apollo),
+  new God('Mercury', Mercury),
+  new God('Bellona', Bellona),
+  new God('He Bo', Hebo)
+];
 
 export default gods;
