3d5b16ee99425aaed00e43b3890de6e038e869fa
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-11-01 15:22:12 -0300

    Remove unused props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,10 @@
 import '../styles/Card.css';
 
-function Card(props: {
-  img: string;
-  name: string;
-  clicked: Function;
-  id: string;
-}) {
-  const { img, name, clicked, id } = props;
+function Card(props: { img: string; name: string; clicked: Function }) {
+  const { img, name, clicked } = props;
 
   return (
-    <div onClick={() => clicked(id)} className='card'>
+    <div onClick={() => clicked(name)} className='card'>
       <img src={img} alt={name + ' character art'} className='card-img' />
       <div className='card-text'>{name}</div>
     </div>
