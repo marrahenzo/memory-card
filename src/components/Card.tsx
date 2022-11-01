fe6300f9420e129a0a8f3291f41b3f52133f1177
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-11-01 20:02:07 -0300

    Add state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,18 @@
+import { useState, useEffect } from 'react';
 import '../styles/Card.css';
 
 function Card(props: { img: string; name: string; clicked: Function }) {
   const { img, name, clicked } = props;
+  const [wasClicked, setWasClicked] = useState(false);
+
+  //Sends false when clicked for the first time and true for the second
+  function handleClick() {
+    clicked(wasClicked);
+    setWasClicked(true);
+  }
 
   return (
-    <div onClick={() => clicked(name)} className='card'>
+    <div onClick={() => handleClick} className='card'>
       <img src={img} alt={name + ' character art'} className='card-img' />
       <div className='card-text'>{name}</div>
     </div>
