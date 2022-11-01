c2e8225775603bb340dad08dc7d48a3e093d843a
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-11-01 20:03:04 -0300

    Remove unused import

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import '../styles/Card.css';
 
 function Card(props: { img: string; name: string; clicked: Function }) {
